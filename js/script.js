c6beae001d29f6b0477434089b9ca23a50d5eda0
francesca-campana <franx3rox@gmail.com>
2020-06-25 17:47:30 +0200

    box dinamici

--- js/script.js
+++ js/script.js
@@ -3,8 +3,23 @@
 // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
 // Il numero ottenuto appare al centro del quadrato
 $(document).ready(function () {
+
+//Creo dinamivamente i quadrati
+  var numeroQuadrati = 36;
+  for (var i = 0; i < numeroQuadrati; i++) {
+
+    var source = $("#box-template").html();
+    var template = Handlebars.compile(source);
+
+    var context = $('.box');
+    var html = template(context);
+    $('.wrapper').append(html);
+
+  }
+
   //Ad ogni click
-  $('.box').click(function () {
+  $(document).on('click','.box',
+    function () {
     var boxCliccato = $(this);
     //parte una chiamata AJAX
 
@@ -15,14 +30,21 @@ $(document).ready(function () {
           success: function (data, stato) {
             var numeroClick = data.response;
             console.log(numeroClick);
+            // Se è <= 5
             if (numeroClick <= 5 ) {
 
+              // il quadrato diventa giallo
+              boxCliccato.removeClass('bg_green');
               boxCliccato.addClass('bg_yellow').text(numeroClick);
-              
 
+
+            // Se è > di 5
             }else if (numeroClick > 5) {
 
+              // il quadrato diventa verde
+              boxCliccato.removeClass('bg_yellow');
               boxCliccato.addClass('bg_green').text(numeroClick);
+
             } else {
 
             }
