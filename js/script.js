fc665d8b9943b080d03b477bc01abfa9e08d75b7
francesca-campana <franx3rox@gmail.com>
2020-06-25 16:33:47 +0200

    Ajax

--- js/script.js
+++ js/script.js
@@ -3,8 +3,31 @@
 // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
 // Il numero ottenuto appare al centro del quadrato
 $(document).ready(function () {
+  //Ad ogni click
   $('.box').click(function () {
-    // alert('test');
+    var boxCliccato = $(this);
+    //parte una chiamata AJAX
+    $.ajax(
+      {
+        url: "https://flynn.boolean.careers/exercises/api/random/int",
+        method: "GET",
+        success: function (data, stato) {
+          var numeroClick = data.response;
+          console.log(numeroClick);
+          if (numeroClick <= 5 ) {
+            $(boxCliccato).addClass('bg_yellow').text(numeroClick);
+
+          }if (numeroClick > 5) {
+            $(boxCliccato).addClass('bg_green').text(numeroClick);
+          } else {
+
+          }
+        },
+        error: function (richiesta, stato, errori) {
+        alert("E' avvenuto un errore. " + errori);
+        }
+
+      });
   });
 
 });
